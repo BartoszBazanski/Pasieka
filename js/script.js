3c6bac3ad02b8b88bb7f55fc59bcf035f60381db
Bartosz Bazański <bazanski.bartosz@gmail.com>
2016-01-24 23:08:19 +0100

    Add preventDefault to scrolling function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var main = function(){
     var scrollTo = function(clickOn, goTo){
-        $(clickOn).click(function(){
+        $(clickOn).click(function(event){
+            event.preventDefault();
             $("html, body").animate({
                 scrollTop: $(goTo).offset().top
             }, 1000);
