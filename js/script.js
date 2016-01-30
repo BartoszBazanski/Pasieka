7f04bb7dd5725157fccc22f2999bd030ee3aec7f
Bartosz Bazanski <bazanski.bartosz@gmail.com>
2016-01-30 10:51:47 +0100

    Add elements to cache

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 var main = function(){
+    var goToAbout = $("#goToAbout");
+    var about = $("#about");
+    var goToHoney = $("#goToHoney");
+    var honey = $("#honey");
+    var goToOffer = $("#goToOffer");
+    var offer = $("#offer");
+    var goToContact = $("#goToContact");
+    var contact = $("#contact");
+    var brand = $(".navbar-brand");
     var scrollTo = function(clickOn, goTo){
-        $(clickOn).click(function(event){
+        clickOn.click(function(event){
             event.preventDefault();
             $("html, body").animate({
-                scrollTop: $(goTo).offset().top
+                scrollTop: goTo.offset().top
             }, 1000);
         });
     };
-    scrollTo("#goToAbout", "#about");
-    scrollTo("#goToHoney", "#honey");
-    scrollTo("#goToOffer", "#offer");
-    scrollTo("#goToContact", "#contact");
-    scrollTo(".navbar-brand", "#about");
+    scrollTo(goToAbout, about);
+    scrollTo(goToHoney, honey);
+    scrollTo(goToOffer, offer);
+    scrollTo(goToContact, contact);
+    scrollTo(brand, about);
 };
 $(document).ready(main);
